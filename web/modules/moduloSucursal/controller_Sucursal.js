b3baf54ef1fe2bd0f49fd1b0c69865e025b9f0a7
Uriel444-web <urielhenandez123@gmail.com>
2024-10-12 18:09:19 -0600

    Update controller_Sucursal.js

--- web/modules/moduloSucursal/controller_Sucursal.js
+++ web/modules/moduloSucursal/controller_Sucursal.js
@@ -78,7 +78,8 @@ function loadTabla() {
 export function selectSucursal(index) {
     let sucursal = sucursales[index];
     document.getElementById("txtImg").value = sucursal.foto;
-    document.getElementById("txtNumUnico").disabled=true = sucursales[index].numero_unico_sucursal;
+    document.getElementById("txtNumUnico").disabled=true;
+    document.getElementById("txtIdAlimento").value = sucursales[index].numero_unico_sucursal;
     document.getElementById("txtNombre").value = sucursales[index].nombre;
     document.getElementById("txtCalleYNum").value = sucursales[index].calleYNum;
     document.getElementById("txtColonia").value = sucursales[index].colonia;
